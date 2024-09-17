e89ed1faa3aede1f3897562609d05b8b8f0d1d4a
Divyasingh03 <divya954.singh@gmail.com>
2024-09-17 15:34:52 +0530

    Update Code.sol

--- Code.sol
+++ Code.sol
@@ -6,60 +6,59 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    struct PlayerItems {
-        uint Bracelets;
-        uint Chain ;
-        uint Bangles ;
-        uint Rings;
+    struct Inventory {
+        uint bronzeCoin;
+        uint copperCoin;
+        uint crystal;
+        uint elixir;
     }
 
-    mapping(address => PlayerItems) public playerItems;
+    mapping(address => Inventory) public playerInventory;
 
     // Numerical identifiers for items
-    uint constant Bracelets = 1;
-    uint constant Chain= 2;
-    uint constant Bangles= 3;
-    uint constant  Rings = 4;
+    uint constant BRONZE_COIN = 1;
+    uint constant COPPER_COIN = 2;
+    uint constant CRYSTAL = 3;
+    uint constant ELIXIR = 4;
 
     // Events
-    event Minted(address indexed to, uint amount);
-    event TokensTransferred(address indexed from, address indexed to, uint amount);
-    event ItemRedeemed(address indexed player, uint itemId, uint price);
-    event TokensBurned(address indexed from, uint amount);
+    event MintCompleted(address indexed receiver, uint quantity);
+    event TokensMoved(address indexed sender, address indexed receiver, uint quantity);
+    event AssetRedeemed(address indexed player, uint assetId, uint cost);
+    event TokensDestroyed(address indexed sender, uint quantity);
 
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
-    function mint(address _to, uint amt) external onlyOwner {
-        _mint(_to, amt);
-        emit Minted(_to, amt);
+    function mint(address recipient, uint quantity) external onlyOwner {
+        _mint(recipient, quantity);
+        emit MintCompleted(recipient, quantity);
     }
 
-// 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-    function transferTokens(address _to, uint amt) public {
-        require(amt <= balanceOf(msg.sender), "Insufficient balance");
-        _transfer(msg.sender, _to, amt);
-        emit TokensTransferred(msg.sender, _to, amt);
+    function transferTokens(address recipient, uint quantity) public {
+        require(quantity <= balanceOf(msg.sender), "Insufficient balance");
+        _transfer(msg.sender, recipient, quantity);
+        emit TokensMoved(msg.sender, recipient, quantity);
     }
 
-    function redeemItem(uint _itemId, uint _price) public {
-        require(_itemId >= Bracelets && _itemId <=  Rings, "Invalid item ID");
-        require(balanceOf(msg.sender) >= _price, "Insufficient balance");
+    function redeemAsset(uint assetId, uint cost) public {
+        require(assetId >= BRONZE_COIN && assetId <= ELIXIR, "Invalid asset ID");
+        require(balanceOf(msg.sender) >= cost, "Insufficient balance");
 
-        if (_itemId == Bracelets) {
-            playerItems[msg.sender].bracelets+= 1;
-        } else if (_itemId == Chain) {
-            playerItems[msg.sender].chain+= 1;
-        } else if (_itemId ==Bangles ) {
-            playerItems[msg.sender].bangles += 1;
-        } else if (_itemId == Rings) {
-            playerItems[msg.sender].rings += 1;
+        if (assetId == BRONZE_COIN) {
+            playerInventory[msg.sender].bronzeCoin += 1;
+        } else if (assetId == COPPER_COIN) {
+            playerInventory[msg.sender].copperCoin += 1;
+        } else if (assetId == CRYSTAL) {
+            playerInventory[msg.sender].crystal += 1;
+        } else if (assetId == ELIXIR) {
+            playerInventory[msg.sender].elixir += 1;
         }
 
-        _burn(msg.sender, _price);
-        emit ItemRedeemed(msg.sender, _itemId, _price);
+        _burn(msg.sender, cost);
+        emit AssetRedeemed(msg.sender, assetId, cost);
     }
 
-    function checkBalance() public view returns (uint) {
+    function viewBalance() public view returns (uint) {
         return balanceOf(msg.sender);
     }
 }
